8ae36dfcf2d413756d964de968cf79257357dba9
Thiago Araujo <thiago_amacedo@hotmail.com>
2022-09-13 21:34:24 -0300

    splash, home and some components

--- App.tsx
+++ App.tsx
@@ -1,47 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { TouchableOpacity, StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'react-native';
+import { useFonts, 
+  Inter_400Regular, 
+  Inter_600SemiBold, 
+  Inter_700Bold, 
+  Inter_900Black 
+} from '@expo-google-fonts/inter';
+import { Background } from './src/components/Background';
+import { Home } from './src/screens/Home';
+import { Loading } from './src/components/Loading';
 
-interface ButtonProps {
-  title: string;
-}
-
-function Button(props: ButtonProps) {
-  return (
-    <View style={styles.button}>
-      <TouchableOpacity> 
-        <Text>
-          {props.title}
-        </Text>
-      </TouchableOpacity>
-    </View>
-  );
-}
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular, 
+    Inter_600SemiBold, 
+    Inter_700Bold, 
+    Inter_900Black   
+  });
+
   return (
-    <View style={styles.container}>
-      <Button title='Clique aqui' />
-      <Button title='Clique aqui 2' />
-      <Button title='Clique aqui 3' />
-      <Button title='Hello world' />
-      <StatusBar style="auto" />
-    </View>
+    <Background>
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <Home /> : <Loading />}
+    </Background>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-
-  button: {
-    backgroundColor: '#34c3eb',
-    borderRadius: 10,
-    padding: 20,
-    margin: 20
-  }
-
-});
